7e3c1f9176d2759e1ed9ee667d407853ce006a4a
chapterjason <jason@sourecode.dev>
2021-09-15 08:59:30 +0200

    enhance(token): Improve token test

--- Tests/TokenTest.php
+++ Tests/TokenTest.php
@@ -1,6 +1,14 @@
 <?php
+/*
+ * This file is part of the SoureCode package.
+ *
+ * (c) Jason Schilling <jason@sourecode.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-namespace SoureCode\Component\Token\Tests\Model;
+namespace SoureCode\Component\Token\Tests;
 
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
